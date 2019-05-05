ba5614ba22639c943ae4731614428a2bc47ab25b
Sergei Chestakov <schestakov@ucdavis.edu>
2019-05-05 16:19:29 -0700

    Add countdown component

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CountDown from 'react-native-countdown-component';
 
+// May 10th
+const SERGEIS_BIRTHDAY = new Date(2019, 4, 10);
 export default class App extends React.Component {
   render() {
+    const now = new Date();
+    const seconds_left = Math.floor((SERGEIS_BIRTHDAY - (now))/1000);
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Time until Sergei's Birthday</Text>
+        <CountDown
+          until={seconds_left}
+          onFinish={() => alert('Time to turn up!')}
+          size={20}
+        />
       </View>
     );
   }
@@ -17,5 +27,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    paddingBottom: 10,
   },
 });
