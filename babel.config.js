d98ee5e200e1009575250d2f49992782e3fc1986
Evan Bacon <baconbrix@gmail.com>
2019-05-08 00:58:56 -0700

    Added web

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [['babel-preset-expo', { web: { allowCommonJS: true } }]],
+    plugins: [
+      [
+        '@babel/plugin-transform-modules-commonjs',
+        {
+          allowTopLevelThis: true,
+        },
+      ],
+    ],
   };
 };
