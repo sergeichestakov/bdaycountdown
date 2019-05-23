065297fc3861507cc376d0d99cd174c83edff98a
Sergei Chestakov <schestakov@ucdavis.edu>
2019-05-22 22:50:58 -0700

    Show how old Ill be on my next bday

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import CountDown from 'react-native-countdown-component';
 
 const MAY = 4;
 const TENTH = 10;
+const NINETEEN_NINETY_EIGHT = 1998;
 
 export default class App extends React.Component {
   render() {
     const now = new Date();
     const currYear = now.getFullYear();
-    const afterBday = now.getMonth() > MAY || (now.getMonth() == MAY && now.getDate() > TENTH);
+    const bdayHasPast = now.getMonth() > MAY || (now.getMonth() == MAY && now.getDate() > TENTH);
 
-    const nextBday = new Date(afterBday ? currYear + 1 : currYear, MAY, TENTH);
+    const nextBday = new Date(bdayHasPast ? currYear + 1 : currYear, MAY, TENTH);
     const secondsLeft = Math.floor((nextBday - (now))/1000);
+
+    const age = currYear - NINETEEN_NINETY_EIGHT + bdayHasPast;
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Time until Sergei's 21st Birthday</Text>
+        <Text style={styles.text}>Time until Sergei turns {age}</Text>
         <CountDown
           until={secondsLeft}
           onFinish={() => alert('Time to turn up!')}
